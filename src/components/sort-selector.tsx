719e0f1a075122095e16f605a9e98c912c075d6c
adrian-burkhart <adrian.burkhart@gmail.com>
2023-01-29 22:33:42 +0100

    Make Selector UI reusable

--- src/components/sort-selector.tsx
+++ src/components/sort-selector.tsx
@@ -1,10 +1,33 @@
-import { SortSelectorUi, SortSelectorUiProps } from '../ui'
+import { SelectorUi, SelectorUiProps } from '../ui'
 
-interface SortSelectorProps extends SortSelectorUiProps {}
+type SortMethod = 'ascending' | 'descending' | 'default'
+
+const options: SelectorUiProps['options'] = [
+  { label: 'Standard', value: 'default' },
+  { label: 'Preis: Aufsteigend', value: 'ascending' },
+  { label: 'Preis: Absteigend', value: 'descending' },
+]
+
+const label = 'Sortieren nach:'
+interface SortSelectorProps extends SelectorUiProps {
+  selectedSortMethod: SortMethod
+  setSelectedSortMethod: (sortMethod: SortMethod) => void
+}
 
 export const SortSelector = ({
   selectedSortMethod,
   setSelectedSortMethod,
 }: SortSelectorProps) => {
-  return <SortSelectorUi {...{ selectedSortMethod, setSelectedSortMethod }} />
+  const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedSortMethod(e.target.value as SortMethod)
+  }
+
+  return (
+    <SelectorUi
+      controlledValue={selectedSortMethod}
+      onChange={onChange}
+      label={label}
+      options={options}
+    />
+  )
 }
---
+++ src/ui/selector.tsx
@@ -0,0 +1,37 @@
+import { Box, Flex, Select } from '@chakra-ui/react'
+
+export interface SelectorUiProps {
+  controlledValue: string
+  label: string
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
+  options: { label: string; value: string }[]
+}
+
+export const SelectorUi = ({
+  controlledValue,
+  label,
+  onChange,
+  options,
+}: SelectorUiProps) => {
+  return (
+    <Flex
+      maxW='sm'
+      mb={4}
+      mx='auto'
+      direction={'row'}
+      justify='space-around'
+      align='center'
+    >
+      <Box w='100%'>
+        <label htmlFor='sort'>{label}</label>
+      </Box>
+      <Select value={controlledValue} onChange={onChange}>
+        {options.map(option => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </Select>
+    </Flex>
+  )
+}
